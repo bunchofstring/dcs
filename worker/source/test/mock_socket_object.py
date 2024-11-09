8e27185991f969e25bb13c9e368c629593817b73
Dave Mitchell <dave@bunchofstring.com>
2024-11-09 15:56:02 -0500

    Update comment for clarity

--- worker/source/test/mock_socket_object.py
+++ worker/source/test/mock_socket_object.py
@@ -2,8 +2,7 @@
 
 # Could be achieved via mocker, but this class is test-framework-agnostic
 class MockSocketObject:
-    # Not strictly necessary, but certainly improves speed and isolation
-    # from the host system state (which is irrelevant to this test)
+    # Not strictly necessary, but improves performance
     @staticmethod
     def bind(_):
         pass
